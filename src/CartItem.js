612e02c92b63a28cbf2b1a0f740253cd90b9b0bd
shreeraj-shinde <shreerajshinde86@gmail.com>
2023-02-14 14:14:51 +0530

    price updation required

--- src/CartItem.js
+++ src/CartItem.js
@@ -3,14 +3,21 @@ import React from "react";
 
 const CartItem = (props) => {
   const [count ,SetCount] = useState(0)
+  const [title ,SetTitle] = useState("Mobile Phone")
+  const[price,SetPrice] = useState(999)
+  const incQty = () =>{
+    var nprice;
+    SetCount(count+1);
+    SetPrice(nprice = nprice + price);
+  }
   return (
     <div className="cart-item">
       <div className="left-block">
         <img style={styles.image} alt="ItemImage" />
       </div>
       <div className="right-block">
-        <div style={{ fintSize: 25 }}>Product Name : {props.title}</div>
-        <div style={{ color: "#777" }}>Price : {props.price} </div>
+        <div style={{ fintSize: 25 }}>Product Name : {title}</div>
+        <div style={{ color: "#777" }}>Price : {price} </div>
         <div style={{ color: "#777" }}>Quantity : {count}</div>
         <div className="cart-item-actions">
           {/**Buttons */}
@@ -18,7 +25,7 @@ const CartItem = (props) => {
             alt="increase"
             src="https://cdn-icons-png.flaticon.com/512/992/992651.png"
             className="action-icons"
-            onClick={()=>SetCount(count+1)}
+            onClick={incQty}
           />
           <img
             alt="decrease"
